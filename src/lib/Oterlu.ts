5022bae4bf520bbe722345efc299251509d0a01c
Jonathan <54381371+axisiscool@users.noreply.github.com>
2022-01-28 20:25:35 -0500

    fix(requests): not properly sending classify request

--- src/lib/Oterlu.ts
+++ src/lib/Oterlu.ts
@@ -1,5 +1,8 @@
 import fetch from 'petitio';
+import { platform, release } from 'node:os';
 import { type OterluErrorMessages, type OterluReturnType, OterluErrors } from './OterluTypes';
+
+const userAgent = `Oterlu-API (petitio) ${platform()}/${release()} (https://github.com/axisiscool/oterlu-api)`;
 
 /**
  * The stored API Key for Oterlu.
@@ -15,7 +18,13 @@ let apiKey: string;
 export async function classifyContent(content: string) {
 	if (content.length > 500) throw new Error(OterluErrors.ContentTooLong);
 
-	const response = await fetch('https://classify.oterlu.com/v1/text').header('x-api-key', apiKey).body({ text: content }, 'json').send();
+	const response = await fetch('https://classify.oterlu.com/v1/text', 'POST')
+		.header({
+			'x-api-key': apiKey,
+			'User-Agent': userAgent
+		})
+		.body({ text: content })
+		.send();
 	const responseAsJson = response.json<OterluReturnType>();
 
 	if (response.statusCode !== 200) throw new Error(`Oterlu API Error: ${parseApiError(responseAsJson.message)}`);
